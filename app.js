702ca45be5905f0cda808016e7e5a941aa6b89bc
Mohit Kumar <mhtkmr54@gmail.com>
2015-03-20 18:40:22 +0530

    Guess what?? product gallery seperated same way as product tabs

--- app.js
+++ app.js
@@ -49,12 +49,21 @@ gen
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(imageNumber){
+  app.directive("productGallery", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-gallery.html",
+      controller: function() {    
+      this.current = 0;
+      this.setCurrent = function(imageNumber){
       this.current = imageNumber || 0;
     };
+    },
+      controllerAs : 'gallery'
+
+    };
   });
+
 
   app.controller('StoreController', function() {
     this.products = gems;
